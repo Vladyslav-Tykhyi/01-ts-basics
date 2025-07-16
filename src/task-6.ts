227f98c2917c025c4832d78d63714aa05b7b6fae
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2025-07-16 17:27:51 +0200

    fixing

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,9 +2,9 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement(<number[]>[1, 2, 3]);
-getFirstElement(<string[]>["a", "b", "c"]);
-getFirstElement(<boolean[]>[true, false]);
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false]);
 
 // console.log(getFirstElement([1, 2, 3] as number[]));
 // console.log(getFirstElement(["a", "b", "c"] as string[]));
